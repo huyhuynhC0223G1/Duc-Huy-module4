23e6fcbc5e3466d06c9f368bbea27227ab10ba88
huyhuynhC0223G1 <huynhduchuy403@gmail.com>
2023-06-17 17:13:02 +0700

    bài tập ngày 16/06

--- ss2_spring_controller/bai_tap/caculator/src/main/java/com/example/caculator/service/impl/CaculatorService.java
+++ ss2_spring_controller/bai_tap/caculator/src/main/java/com/example/caculator/service/impl/CaculatorService.java
@@ -8,29 +8,29 @@ public class CaculatorService implements ICaculatorService {
     @Override
     public String resultCaculator(double number1, double number2, String calculate) {
         String result = null;
-        double a = 0;
+        double result1 = 0;
         if (number1 == 0 || number2 == 0) {
             result = "you must enter the full number";
         } else {
             switch (calculate) {
                 case "+":
-                    a = number1 + number2;
-                    result = Double.toString(a);
+                    result1 = number1 + number2;
+                    result = Double.toString(result1);
                     break;
                 case "-":
-                    a = number1 - number2;
-                    result = Double.toString(a);
+                    result1 = number1 - number2;
+                    result = Double.toString(result1);
                     break;
                 case "*":
-                    a = number1 * number2;
-                    result = Double.toString(a);
+                    result1 = number1 * number2;
+                    result = Double.toString(result1);
                     break;
                 case "/":
                     if (number2 == 0) {
                         result = "not divided by 0";
                     } else {
-                        a = number1 / number2;
-                        result = Double.toString(a);
+                        result1 = number1 / number2;
+                        result = Double.toString(result1);
                     }
                     break;
                 default:
