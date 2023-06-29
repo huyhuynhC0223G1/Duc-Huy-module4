4fda513f94e2c3eaf6e705d6b4c79081b1d12465
huyhuynhC0223G1 <huynhduchuy403@gmail.com>
2023-06-29 19:08:02 +0700

    bai tap ngay 29/06

---
+++ ss10_cookie_session/bai_tap/gio_hang/src/main/java/com/example/gio_hang/controller/ShoppingCartController.java
@@ -0,0 +1,23 @@
+package com.example.gio_hang.controller;
+
+import com.example.gio_hang.model.Cart;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ShoppingCartController {
+    @ModelAttribute("cart")
+    public Cart setupCart() {
+        return new Cart();
+    }
+
+    @GetMapping("/shopping-cart")
+    public String showCart(@SessionAttribute("cart") Cart cart, Model model) {
+        model.addAttribute("cart", cart);
+        return "/cart";
+    }
+}
